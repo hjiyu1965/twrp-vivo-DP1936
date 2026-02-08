a08128f38d95c6225714dedc5a5fe6a204e01b44
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-02-08 22:37:15 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,12 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_PD1936.mk
+    $(LOCAL_DIR)/twrp_PD1936.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_PD1936-user \
-    omni_PD1936-userdebug \
-    omni_PD1936-eng
+  twrp_PD1936-eng
