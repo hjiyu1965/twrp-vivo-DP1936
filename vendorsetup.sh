be374cec0a2dea659cf6459f43dd5edd84ddaec9
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-02-08 22:37:40 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_PD1936-user
-add_lunch_combo omni_PD1936-userdebug
-add_lunch_combo omni_PD1936-eng
+export ALLOW_MISSING_DEPENDENCIES=true
